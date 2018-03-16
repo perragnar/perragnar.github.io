62e6c9d5ff76e9d289a3d100c735bba4e4a0af9d
Per Ragnar Edin <perragnar@gmail.com>
2018-03-16 10:14:35 +0100

    Add element animation. Yay or nay?

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,40 @@
-'use strict';
+$(function() {
 
-(function (window, document, undefined) {
-    // Code goes here
-})(window, document);
+    var $window = $(window);
+    
+    // Which elements to animate
+    var animatedElements = $('.animate');
+    
+    animateElements();
+
+    window.onscroll = function (e) {
+        animateElements();
+    };
+
+    function animateElements() {
+        if (animatedElements.length) {
+            var window_height = $window.height();
+            var window_top_position = $window.scrollTop();
+            var window_mid = window_top_position + $window.height() / 2;
+            var window_bottom_position = (window_top_position + window_height);
+            var window_top_offset = 0; //window_height * 0.25;
+            var window_bottom_offset = 0; //window_height * 0.05;
+
+            var i = 1;
+            $.each(animatedElements, function () {
+                var $element = $(this);
+                var element_height = $element.outerHeight();
+                var element_top_position = $element.offset().top;
+                var element_bottom_position = (element_top_position + element_height);
+                
+                if ((element_bottom_position >= window_top_position + window_top_offset) && (element_top_position <= window_bottom_position - window_bottom_offset)) {
+                    $element.addClass('animate--in-view animate--animated');
+                } else {
+                    $element.removeClass('animate--in-view');
+                }
+                i++;
+            });
+        }
+    }
+
+});
