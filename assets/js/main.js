68275e44ae33795595f49ccf0c2f74cee1d7e57c
Per Ragnar Edin <perragnar@gmail.com>
2018-04-27 12:15:50 +0200

    WIP

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,42 @@
 var $window = $(window);
 
+// Window scroll data
+var window_height = $window.height();
+var window_top_position = $window.scrollTop();
+var window_mid = window_top_position + $window.height() / 2;
+var window_bottom_position = (window_top_position + window_height);
+
 // Which elements to animate
 var animatedElements = $('.animate');
+var galleryPhotos = $('.photo-gallery .photo');
 
-animateElements();
+animateElements(window_height, window_top_position, window_mid, window_bottom_position);
 
 window.onscroll = function (e) {
-    animateElements();
+    updateScrollData();
+
+    if(animatedElements.length) {
+        animateElements(window_height, window_top_position, window_mid, window_bottom_position);
+    }
+
+    // if(galleryPhotos.length) {
+    //     handlePhotoData(window_top_position);
+    // }
 };
 
-function animateElements() {
+function updateScrollData() {
+    window_height = $window.height();
+    window_top_position = $window.scrollTop();
+    window_mid = window_top_position + $window.height() / 2;
+    window_bottom_position = (window_top_position + window_height);
+}
+
+function animateElements(window_height, window_top_position, window_mid, window_bottom_position) {
     if (animatedElements.length) {
-        var window_height = $window.height();
-        var window_top_position = $window.scrollTop();
-        var window_mid = window_top_position + $window.height() / 2;
-        var window_bottom_position = (window_top_position + window_height);
         var window_top_offset = 0; //window_height * 0.25;
         var window_bottom_offset = window_height * 0.05;
-
         var i = 1;
+        
         $.each(animatedElements, function () {
             var $element = $(this);
             var element_height = $element.outerHeight();
@@ -33,4 +51,17 @@ function animateElements() {
             i++;
         });
     }
+}
+
+function handlePhotoData(window_top_position) {
+    // $.each(galleryPhotos, function () {
+    //     var $element = $(this).find('.photo__data');
+    //     var element_top_position = $element.offset().top;
+
+    //     console.log(element_top_position, window_top_position);
+
+    //     if(element_top_position <) {
+
+    //     }
+    // });
 }
