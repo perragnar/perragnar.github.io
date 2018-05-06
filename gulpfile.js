80379de943bf2a136a79afba792ee76c7535e000
Per Ragnar Edin <perragnar@gmail.com>
2018-05-06 23:59:49 +0200

    Upsized the image resolutions

--- gulpfile.js
+++ gulpfile.js
@@ -43,7 +43,7 @@ gulp.task('handleInbox', () => {
         .pipe(rename((path) => {
             createImgPath(path);
         }))
-        .pipe(resize({ width: 1200 }))
+        .pipe(resize({ width: 2000 }))
         .pipe(imagemin([imagemin.jpegtran({ progressive: true })]))
         .pipe(gulp.dest('./assets/photos/galleries/large/'))
 
@@ -51,7 +51,7 @@ gulp.task('handleInbox', () => {
         .pipe(rename((path) => {
             createImgPath(path);
         }))
-        .pipe(resize({ width: 600 }))
+        .pipe(resize({ width: 1200 }))
         .pipe(imagemin([imagemin.jpegtran({ progressive: true })]))
         .pipe(gulp.dest('./assets/photos/galleries/medium/'))
 
