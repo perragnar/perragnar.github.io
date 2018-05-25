b139a2c8effeba671d9b0f5aec0b81f8889085b8
Per Ragnar Edin <perragnar@gmail.com>
2018-05-25 23:00:50 +0200

    Removing thumbs and making photos smaller to save space

--- gulpfile.js
+++ gulpfile.js
@@ -43,7 +43,7 @@ gulp.task('handleInbox', () => {
         .pipe(rename((path) => {
             createImgPath(path);
         }))
-        .pipe(resize({ width: 1800 }))
+        .pipe(resize({ width: 1200 }))
         .pipe(imagemin([imagemin.jpegtran({ progressive: true })]))
         .pipe(gulp.dest('./assets/photos/galleries/large/'))
 
@@ -51,17 +51,18 @@ gulp.task('handleInbox', () => {
         .pipe(rename((path) => {
             createImgPath(path);
         }))
-        .pipe(resize({ width: 1200 }))
+        .pipe(resize({ width: 800 }))
         .pipe(imagemin([imagemin.jpegtran({ progressive: true })]))
         .pipe(gulp.dest('./assets/photos/galleries/medium/'))
 
         // Thumb
-        .pipe(rename((path) => {
-            createImgPath(path);
-        }))
-        .pipe(resize({ width: 600, height: 600, crop: true, upscale: true }))
-        .pipe(imagemin([imagemin.jpegtran({ progressive: true })]))
-        .pipe(gulp.dest('./assets/photos/galleries/thumbs/'))
+        // .pipe(rename((path) => {
+        //     createImgPath(path);
+        // }))
+        // .pipe(resize({ width: 600, height: 600, crop: true, upscale: true }))
+        // .pipe(imagemin([imagemin.jpegtran({ progressive: true })]))
+        // .pipe(gulp.dest('./assets/photos/galleries/thumbs/'))
+        
         ;
 });
 
