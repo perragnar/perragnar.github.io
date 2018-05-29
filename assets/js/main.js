103bfe09aa4e99da4ffd670a602b88fa209ee450
Per Ragnar Edin <perragnar@gmail.com>
2018-05-29 15:52:36 +0200

    Photos are smaller on retina screens

--- assets/js/main.js
+++ assets/js/main.js
@@ -65,6 +65,15 @@ var defaults = {
     //   "rotate'
     //
     // transitionEffect: "fade",
+
+    afterLoad : function(instance, current) {
+        var pixelRatio = window.devicePixelRatio || 1;
+
+        if ( pixelRatio > 1.5 ) {
+            current.width  = current.width  / pixelRatio;
+            current.height = current.height / pixelRatio;
+        }
+    }
 };
 
 $('[data-fancybox').fancybox(defaults);
